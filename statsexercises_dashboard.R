54b0a441175bd15b99c04e8330ab3f5245611ed3
cknotz <carlo.knotz@gmail.com>
2021-05-17 17:38:13 +0200

    Fixed problem w/ correlation coefficient; added t-test; draft complete

--- statsexercises_dashboard.R
+++ statsexercises_dashboard.R
@@ -19,6 +19,7 @@ ui <- dashboardPage(
   dashboardSidebar(collapsed = T,
     sidebarMenu(
       menuItem("Start",tabName = "start"),
+      menuItem("Mathematical notation", tabName = "math"),
       menuItem("Measures of central tendency",tabName = "cent"),
       menuItem("Measures of spread",tabName = "spread"),
       menuItem("Chi-squared test",tabName = "chi"),
@@ -80,7 +81,7 @@ ui <- dashboardPage(
               )),
               fluidRow(
                 box(width = 12,title = "The detailed solution",collapsible = T,
-                    collapsed = F,solidHeader = F,id = "det",
+                    collapsed = T,solidHeader = F,
                     uiOutput("cor_detail1"),
                     uiOutput("cor_detail2"),
                     uiOutput("cor_detail3"),
@@ -91,7 +92,8 @@ ui <- dashboardPage(
                     uiOutput("cor_detail6"),
                     uiOutput("cor_detail7"),
                     uiOutput("cor_detail8"),
-                    uiOutput("cor_detail9")
+                    uiOutput("cor_detail9"),
+                    uiOutput("cor_detail10")
                     )
                 
               )
@@ -132,11 +134,12 @@ observeEvent(input$sim, {
 observeEvent(input$solution,{
 
 # Simple solution
-  res <- isolate(round(cor(vals$data$V1,vals$data$V2),digits=2))
+  res <- isolate(round(cor(vals$data$X,vals$data$Y,
+                           method = "pearson"),digits=2))
   
   t <- round((res*sqrt(10-2))/(sqrt(1-res^2)),digits=3)
   
-  p_val <- round(dt(t,df = 10-2),digits = 3)
+  p_val <- round(1 - pt(t, 8, lower.tail = T),digits = 3)
   
   output$result <- renderText(
     paste0("The correlation coefficient is: ",res,", and its t-value is: ",t,".\n
@@ -161,9 +164,9 @@ output$cor_detail2 <- renderUI({
 output$cor_detail3 <- renderUI({
   HTML(paste0("<p>First we calculate the mean values for X and Y, which are:</p>
        
-       <p><strong>Mean of X: ",isolate(round(mean(vals$data$X),digits = 1)),"</strong></p>
-       
-       <p><strong>Mean of Y: ",isolate(round(mean(vals$data$Y),digits = 1)),"</strong></p>"))  
+       <p><strong>Mean of X: ",isolate(round(mean(vals$data$X),digits = 3)),"</strong></p>
+      
+       <p><strong>Mean of Y: ",isolate(round(mean(vals$data$Y),digits = 3)),"</strong></p>"))  
 })
 
 output$cor_detail4 <- renderUI({
@@ -171,8 +174,8 @@ output$cor_detail4 <- renderUI({
                   For example, for the first observation, we get the following results:</p>
               
               
-              <p><strong>Deviation from the mean of X: ",isolate(vals$data[1,c("X")])," - ",isolate(round(mean(vals$data$X),digits = 1)), " = ",isolate(round(vals$data[1,3] - round(mean(vals$data$X),digits = 1),digits=1)),"</strong></p>
-              <p><strong>Deviation from the mean of X: ",isolate(vals$data[1,c("Y")])," - ",isolate(round(mean(vals$data$Y),digits = 1)), " = ",isolate(round(vals$data[1,4] - round(mean(vals$data$Y),digits = 1),digits=1)),
+              <p><strong>Deviation from the mean of X: ",isolate(vals$data[1,c("X")])," - ",isolate(round(mean(vals$data$X),digits = 3)), " = ",isolate(round(vals$data[1,3] - round(mean(vals$data$X),digits = 3),digits=3)),"</strong></p>
+              <p><strong>Deviation from the mean of X: ",isolate(vals$data[1,c("Y")])," - ",isolate(round(mean(vals$data$Y),digits = 3)), " = ",isolate(round(vals$data[1,4] - round(mean(vals$data$Y),digits = 3),digits=3)),
               "</strong></p>
               
               <p>The following table displays the deviations for each of the observations in our
@@ -182,14 +185,14 @@ output$cor_detail4 <- renderUI({
 
 vals$cordata <- vals$data
 
-  vals$cordata$meanX <- isolate(round(mean(vals$cordata$X),digits = 1))
-  vals$cordata$meanY <- isolate(round(mean(vals$cordata$Y),digits = 1)) 
+  vals$cordata$meanX <- isolate(round(mean(vals$cordata$X),digits = 3))
+  vals$cordata$meanY <- isolate(round(mean(vals$cordata$Y),digits = 3)) 
   
-  vals$cordata$deviationX <- isolate(vals$cordata[,c("X")] - round(vals$cordata[,c("meanX")],1))
-  vals$cordata$deviationY <- isolate(vals$cordata[,c("Y")] - round(vals$cordata[,c("meanY")],1))
+  vals$cordata$deviationX <- isolate(vals$cordata[,c("X")] - round(vals$cordata[,c("meanX")],3))
+  vals$cordata$deviationY <- isolate(vals$cordata[,c("Y")] - round(vals$cordata[,c("meanY")],3))
     
 output$cor_tab1 <- renderTable(vals$cordata[,c("X","Y","meanX","meanY","deviationX","deviationY")],
-                             digits = 1,
+                             digits = 3,
                              rownames = T)
   
 output$cor_detail5 <- renderUI({
@@ -197,25 +200,27 @@ output$cor_detail5 <- renderUI({
               
               <p>We start again with the first observation:</p>
               
-              <p><strong>Deviation from X times Deviation from Y: ",isolate(round(vals$cordata[1,c("deviationX")],digits=1))," x ",isolate(round(vals$cordata[1,c("deviationY")],digits=1))," = ",round(round(vals$cordata[1,c("deviationX")],digits=1)*round(vals$cordata[1,c("deviationY")],digits=1),digits=1),"</strong></p>
+              <p><strong>Deviation from X times Deviation from Y: ",isolate(round(vals$cordata[1,c("deviationX")],digits=3))," x ",isolate(round(vals$cordata[1,c("deviationY")],digits=3))," = ",round(round(vals$cordata[1,c("deviationX")],digits=3)*round(vals$cordata[1,c("deviationY")],digits=3),digits=3),"</strong></p>
               
               <p>And, again, we show this for all observation in a table:</p>"))
 })
 
-vals$cordata$DevX_times_DevY <- round(vals$cordata[c("deviationX")],digits=1)*round(vals$cordata[c("deviationY")],digits=1)
+vals$cordata$DevX_times_DevY <- round(vals$cordata[c("deviationX")],digits=3)*round(vals$cordata[c("deviationY")],digits=3)
 
 
 output$cor_tab2 <- renderTable(vals$cordata[,c("X","Y","meanX","meanY","deviationX","deviationY","DevX_times_DevY")],
-                               digits = 1,
+                               digits = 3,
                                rownames = T)
 
 output$cor_detail6 <- renderUI({
   HTML(paste0("<strong>Almost done!</strong> Now that we have the multiplied deviation scores, we simply
-              sum them up over all observations and divide by the number of observations (10).</p>
+              sum them up over all observations and then divide by the number of observations minus 1. <strong>Important:</strong> In the calculation here,
+              we divide by n-1 rather than n; this is because that is the way R computes it, and also because we are dealing here with a quite small sample of 10 observations.
+              In a larger dataset (several hundreds or thousands of observations, this should not make a difference but here it does).</p>
               
-              <p><strong>The sum of all the scores (the rightmost column in the table above) is: ",round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=1),"</strong></p>
+              <p><strong>The sum of all the scores (the rightmost column in the table above) is: ",round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=3),"</strong></p>
               
-              <p><strong>And this divided by the number of observations - the covariance - is: ",round(round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=1)/10,digits=1),"</strong></p>"))
+              <p><strong>And this divided by the number of observations minus 1 - the covariance - is: ",round(round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=3)/9,digits=3),"</strong></p>"))
 })
 
 output$cor_detail7 <- renderUI({
@@ -227,13 +232,37 @@ output$cor_detail7 <- renderUI({
 })
 
 output$cor_detail8 <- renderUI({
-  HTML(paste0("<p>In our example, <strong>the variance of X is: ",round(var(isolate(vals$cordata[,c("X")])),digits = 1),", and the variance of Y is: ",round(var(isolate(vals$cordata[,c("Y")])),digits = 1),"</strong></p>
+  HTML(paste0("<p>In our example, <strong>the variance of X is: ",round(var(isolate(vals$cordata[,c("X")])),digits = 3),", and the variance of Y is: ",round(var(isolate(vals$cordata[,c("Y")])),digits = 3),"</strong></p>
               
               <p>If we now plug these values into the equation above, we get:</p>"))
 })
 
 output$cor_detail9 <- renderUI({
-  HTML(paste0("<p> r = <math><mfrac><mn>",round(round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=1)/10,digits=1),"</mn><msqrt><mn>",round(var(isolate(vals$cordata[,c("X")])),digits = 1),"</mn><mo>*</mo><mn>",round(var(isolate(vals$cordata[,c("X")])),digits = 1),"</mn></msqrt></mfrac></math> =",round(round(round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=1)/10,digits=1)/(sqrt(round(var(isolate(vals$cordata[,c("X")])),digits = 1)*round(var(isolate(vals$cordata[,c("Y")])),digits = 1))),digits = 2),"</p>"))
+  HTML(paste0("<p><strong> r = <math><mfrac><mn>",round(round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=3)/9,digits=3),"</mn><msqrt><mn>",round(var(isolate(vals$cordata[,c("X")])),digits = 3),"</mn><mo>*</mo><mn>",round(var(isolate(vals$cordata[,c("Y")])),digits = 3),"</mn></msqrt></mfrac></math> =",round(round(round(sum(vals$cordata[,c("DevX_times_DevY")]),digits=3)/9,digits=3)/(sqrt(round(var(isolate(vals$cordata[,c("X")])),digits = 3)*round(var(isolate(vals$cordata[,c("Y")])),digits = 3))),digits = 2),"</strong></p>"))
+})
+
+output$cor_detail10 <- renderUI({
+  HTML(paste0("<p>Now that we have the correlation coefficient, we also want to know: Did we get this result simply by random chance?
+       (Technically, the answer is of course yes - the computer generated some random numbers for us. But we will just pretend for now that we have
+       some real data with actual meaning in front of us.)</p>
+       
+       To perform a formal test, we calculate the <strong>t-statistic</strong> for our correlation coefficient. The formula
+       for this calculation looks as follows:</p>
+       
+       <p> <math> <msub><mi>t</mi><mi>r</mi></msub> <mo>=</mo>  <mfrac><mrow> <mi>r</mi><msqrt><mi>n</mi><mo>-</mo><mn>2</mn></msqrt> </mrow> <mrow> <msqrt><mn>1</mn><mo>-</mo><msup><mi>r</mi><mn>2</mn></sup></msqrt></mrow></mfrac></math></p>
+              
+       <p>With our values plugged into the formula, we get:</p>
+              
+       <p> <math> <msub><mi>t</mi><mi>r</mi></msub> <mo>=</mo>  <mfrac><mrow><mn>",isolate(round(cor(vals$cordata$X,vals$cordata$Y,
+                                                                                                 method = "pearson"),digits=2)),"</mn><msqrt><mn>10</mn><mo>-</mo><mn>2</mn></msqrt> </mrow> <mrow> <msqrt><mn>1</mn><mo>-</mo><msup><mn>",isolate(round(cor(vals$cordata$X,vals$cordata$Y,
+                                                                                                                                                                                                                                                            method = "pearson"),digits=2)),"</mn><mn>2</mn></sup></msqrt></mrow></mfrac> <mo>=</mo><mn>",
+              round(isolate(round(cor(vals$cordata$X,vals$cordata$Y,
+                                method = "pearson"),digits=2))*sqrt(10-2)/(sqrt(1-(isolate(round(cor(vals$cordata$X,vals$cordata$Y,
+                                                                                                      method = "pearson"),digits=2)))^2)),digits = 3),"</mn></math></p>
+              
+        <p>R tells us that the corresponding <i>p</i>-value is ",p_val,". Can you confirm that this
+              value makes sense using the table of critical t-values in Appendix B of Kellstedt and Whitten (2013)? (Careful: Their table
+              shows only some selected t- and their corresponding p-values! T-values that are relatively low - i.e. below what is required for a p-value of 0.1 or lower - are not shown.)</p>"))
 })
 
 })
