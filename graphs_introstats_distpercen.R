894b29af714c6b51f61ea14500d8ae82e1cf27b4
cknotz <carlo.knotz@gmail.com>
2021-09-14 17:01:37 +0200

    Added graphs to show both percentiles and corresponding z-scores

--- graphs_introstats_distpercen.R
+++ graphs_introstats_distpercen.R
@@ -76,6 +76,65 @@ ggplot(NULL, aes(c(-2,2))) +
   ggsave("dist_5twotail.pdf")
 
 
+# Percentiles, with z-scores
+############################
+zlev <- .025  
+  
+ggplot(NULL, aes(c(-3,3))) + 
+  geom_area(stat = "function", fun = dnorm, fill = "#d95f02", xlim = c(-3, qnorm(zlev))) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey30", xlim = c(qnorm(zlev), qnorm(1-zlev))) +
+  geom_area(stat = "function", fun = dnorm, fill = "#d95f02", xlim = c(qnorm(1-zlev), 3)) +
+  geom_vline(xintercept = qnorm(1-zlev), color = "#d3d3d3", linetype = "dashed", size=1.5) +
+  geom_vline(xintercept = qnorm(zlev), color = "#d3d3d3", linetype = "dashed", size=1.5) +
+  annotate("segment", x = qnorm(zlev), xend = qnorm(1-zlev), y = dnorm(qnorm(zlev)), yend = dnorm(-qnorm(zlev)), arrow = arrow(ends='both'),
+           size = 1.5, color = "#d3d3d3") +
+  annotate("text", x=0, y=dnorm(qnorm(zlev))+.015,label = paste0(100*(1-2*zlev),"% of data"), color="#d3d3d3", fontface = "bold") +
+  labs(y = "Density", x = "",
+       title = paste0("+/-",round(qnorm(1-zlev),digits = 3)," stand. deviations around the mean")) +
+  scale_x_continuous(breaks = seq(-3,3,1)) +
+  theme_bw() +
+  theme(axis.text = element_text(size=12),
+        plot.title = element_text(face = "bold"))
+  ggsave(paste0("zscores_",100*(1-2*zlev),".pdf"))
+  
+zlev <- .005  
+
+ggplot(NULL, aes(c(-3,3))) + 
+  geom_area(stat = "function", fun = dnorm, fill = "#d95f02", xlim = c(-3, qnorm(zlev))) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey30", xlim = c(qnorm(zlev), qnorm(1-zlev))) +
+  geom_area(stat = "function", fun = dnorm, fill = "#d95f02", xlim = c(qnorm(1-zlev), 3)) +
+  geom_vline(xintercept = qnorm(1-zlev), color = "#d3d3d3", linetype = "dashed", size=1.5) +
+  geom_vline(xintercept = qnorm(zlev), color = "#d3d3d3", linetype = "dashed", size=1.5) +
+  annotate("segment", x = qnorm(zlev), xend = qnorm(1-zlev), y = dnorm(qnorm(zlev)), yend = dnorm(-qnorm(zlev)), arrow = arrow(ends='both'),
+           size = 1.5, color = "#d3d3d3") +
+  annotate("text", x=0, y=dnorm(qnorm(zlev))+.015,label = paste0(100*(1-2*zlev),"% of data"), color="#d3d3d3", fontface = "bold") +
+  labs(y = "Density", x = "",
+       title = paste0("+/-",round(qnorm(1-zlev),digits = 3)," stand. deviations around the mean")) +
+  scale_x_continuous(breaks = seq(-3,3,1)) +
+  theme_bw() +
+  theme(axis.text = element_text(size=12),
+        plot.title = element_text(face = "bold"))
+ggsave(paste0("zscores_",100*(1-2*zlev),".pdf"))
+  
+zlev <- .05  
+
+ggplot(NULL, aes(c(-3,3))) + 
+  geom_area(stat = "function", fun = dnorm, fill = "#d95f02", xlim = c(-3, qnorm(zlev))) +
+  geom_area(stat = "function", fun = dnorm, fill = "grey30", xlim = c(qnorm(zlev), qnorm(1-zlev))) +
+  geom_area(stat = "function", fun = dnorm, fill = "#d95f02", xlim = c(qnorm(1-zlev), 3)) +
+  geom_vline(xintercept = qnorm(1-zlev), color = "#d3d3d3", linetype = "dashed", size=1.5) +
+  geom_vline(xintercept = qnorm(zlev), color = "#d3d3d3", linetype = "dashed", size=1.5) +
+  annotate("segment", x = qnorm(zlev), xend = qnorm(1-zlev), y = dnorm(qnorm(zlev)), yend = dnorm(-qnorm(zlev)), arrow = arrow(ends='both'),
+           size = 1, color = "#d3d3d3") +
+  annotate("text", x=0, y=dnorm(qnorm(zlev))+.015,label = paste0(100*(1-2*zlev),"% of data"), color="#d3d3d3", fontface = "bold") +
+  labs(y = "Density", x = "",
+       title = paste0("+/-",round(qnorm(1-zlev),digits = 3)," stand. deviations around the mean")) +
+  scale_x_continuous(breaks = seq(-3,3,1)) +
+  theme_bw() +
+  theme(axis.text = element_text(size=12),
+        plot.title = element_text(face = "bold"))
+ggsave(paste0("zscores_",100*(1-2*zlev),".pdf"))  
+  
 
 
-
+  
